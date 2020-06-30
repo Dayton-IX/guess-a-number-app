578f72c672d1aabae24306d3ed03421eba1fcbe5
Max Dayton <daytonmaximus@gmail.com>
2020-06-30 14:38:53 -0600

    game over image

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, Button, Alert } from 'react-native';
 
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
+import TitleText from '../components/TitleText';
 import Colors from '../constants/colors';
 
 const generateRandomBetween = (min, max, exclude) => {
@@ -47,7 +48,7 @@ const GameScreen = props => {
 
     return (
         <View style={styles.screen}>
-            <Text>Opponent's Guess</Text>
+            <TitleText>Opponent's Guess</TitleText>
             <NumberContainer>{currentGuess}</NumberContainer>
             <Card style={styles.buttonContainer}>
                 <View style={styles.button}>
